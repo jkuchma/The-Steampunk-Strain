be23bf0bd66b6ae1a7433f13f97b9de9e33f7ec0
PeteAlexLaws <67641743+PeteAlexLaws@users.noreply.github.com>
2021-11-10 02:17:04 -0500

    base commit for main branch before new development fork

--- Assets/Scripts/EnemyBehaviorBase.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-/* placeholder base class for all enemy behaviors; to be inherited by enemy specific classes */
-public class EnemyBehaviorBase : MonoBehaviour
-{
-    EnemyMovementBase MoveController;
-}
