74362ce42c6bf7cf5a74857bead81dd2ae772fa0
PeteAlexLaws <67641743+PeteAlexLaws@users.noreply.github.com>
2021-11-17 17:02:59 -0500

    update with fsm

--- Assets/Scripts/DefaultScript.cs
+++ Assets/Scripts/DefaultScript.cs
@@ -5,21 +5,21 @@ using UnityEngine.SceneManagement;
 ** and is a nice quick hack into Unity to place logic that decides default scene/action behavior when the game starts */
 public class DefaultScript : MonoBehaviour
 {
-    [SerializeField]GameType StartType = GameType.Debug;
+    [SerializeField] GameType startingScene = GameType.Debug;
 
     enum GameType
     {
-        Debug,  // development mode; will load a test scene for developing & debugging code
-        Demo    // prototype mode; will load the demo scene for level building and presentation
+        Debug,  /* development mode; will load a test scene for developing & debugging of code */
+        Demo    /* prototype mode; will load the demo scene for level building and presentation */
     }
 
     void Start()
     { 
-        switch (StartType)
+        switch (startingScene)
         { 
             case(GameType.Demo): SceneManager.LoadScene("DemoScene"); break;
-            case(GameType.Debug): SceneManager.LoadScene("TestScene"); break;
-            default: break;
+            case(GameType.Debug): SceneManager.LoadScene("DebugScene"); break;
+            // default: break;
         }
 
     }
